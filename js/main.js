8eeda3967d51a6025644295e3de8d936e7ea4c2c
Tina Chia <tinachia_5@yahoo.com>
2022-11-23 13:51:57 +0000

    js page-view set up

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var $pagecontainer = document.querySelector('.page-container');
+
+view $page = document.querySelectorAll('.page');
+var $view = document.querySelectorAll('.view');
+
+$pagecontainer.addEventListener('click', pageView);
+
+function pageView (event) {
+  if (!event.target.matches('.page')) {
+    return;
+  }
+  for (var i = 0; i < $page.length; i++) {
+    if ($tab[i] === event.target) {
+      $page[i].className = 'page active';
+    } else {
+      $page[i].className = 'page';
+    }
+  }
+  var dataView = event.target.getAttribute('data-view');
+  for (var j = 0; j < $view.length; j++) {
+    if ($view[j].getAttribute('data-view') === dataView) {
+      $view[j].className = 'view';
+    } else {
+      $view[j].className = 'view hidden';
+    }
+  }
+});
+
+}
